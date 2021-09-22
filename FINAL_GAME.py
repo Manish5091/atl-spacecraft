37ccf4846180c843659648c5d845ce3a6eb22c89
Manish <manishgamer792@gmail.com>
2021-09-22 12:56:30 +0530

    WELCOME SCREEN

--- FINAL_GAME.py
+++ FINAL_GAME.py
@@ -1,13 +1,10 @@
 import pygame
 import random
 
-from pygame.constants import K_1
-
 #pygame.mixer.init()
 #pygame.mixer.music.load('')
 #pygame.mixer.music.play()
 pygame.init()
-
 
 screen_width = 1350
 screen_height = 750
@@ -15,18 +12,14 @@ black =0,0,0
 # Creating Window
 gamewindow = pygame.display.set_mode((screen_width, screen_height))
 
-#Welcome Screen
-wlc = pygame.image.load("1.jpeg")
-pygame.display.update()
-
 #Background Image
-bgimg = pygame.image.load("BACKGROUND.jpg")
+bgimg = pygame.image.load("Back_Ground.jpg")
 bgimg = pygame.transform.scale (bgimg, (screen_width, screen_height)).convert_alpha()
-sprite = pygame.image.load("2.png")
+sprite = pygame.image.load("Spawn_Land.png")
 sprite = pygame.transform.scale (sprite, (1350, 800)).convert_alpha()
+
 pygame.display.update()
 
-sprite_rect = bgimg.get_rect()
 pygame.display.set_caption("SPACECRAFT")
 pygame.display.update()
   
@@ -34,9 +27,6 @@ pygame.display.update()
 exit_game = False 
 game_over = False
 
-vel = 2
-current_img_x = 0
-current_img_y = 0
 # Game Loop
 while not exit_game:
     for event in pygame.event.get () :
@@ -44,19 +34,13 @@ while not exit_game:
         if event.type == pygame.QUIT:
             exit_game = True
 
-            keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_w]:
-            if current_img_y<screen_height:
-               current_img_y +=vel
-            else:
-                bgimg = pygame.image.load("finding-water.jpg")
-                current_img_y=0
 
     gamewindow.fill(black)   
     gamewindow.blit(bgimg,(0,0))
-    gamewindow.blit(sprite,(current_img_x,current_img_y))
+    gamewindow.blit(sprite,(0,0))
     pygame.display.update()     
 
 pygame.quit()
 quit()
+
+
